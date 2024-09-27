b2d54bbaaf2b9e6695215894fe3bfa8321d02a90
Ray Orunsolu <rorunsolu@gmail.com>
2024-09-27 19:28:32 +0100

    changed scss variable sto css variables

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import '/styles/style.scss';
 
+import '/styles/utilities.scss';
+import '/styles/typography.scss';
 import '/styles/navbar.scss';
 import '/styles/header.scss';
 import '/styles/footer.scss';
-
-import '/styles/components/button.scss';
 
 import '/styles/sections/properties.scss';
 import '/styles/sections/listings.scss';
