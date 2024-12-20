9a91d810c2c41e57512068687aa81ef808d30e91
Ray Orunsolu <rorunsolu@gmail.com>
2024-12-20 16:03:24 +0000

    removed comments and fixed other issues

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ import '/styles/menu.scss';
 import '/styles/sections/properties.scss';
 import '/styles/sections/agency.scss';
 import '/styles/sections/benefits.scss';
-import '/styles/sections/city.scss';
 import '/styles/sections/testimonial.scss';
 import '/styles/sections/conversion.scss';
 import '/styles/sections/gallery.scss';
